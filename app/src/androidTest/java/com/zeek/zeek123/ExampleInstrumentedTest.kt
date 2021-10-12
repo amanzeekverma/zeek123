9d840d7e662830a82c3051ca99c0f2172951fe77
Aman Verma <avD.ZeeK@gmail.com>
2021-10-11 21:04:46 -0400

    refactor

--- app/src/androidTest/java/com/zeek/zeek123/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/zeek/zeek123/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.zeek123", appContext.packageName)
+        assertEquals("com.zeek.zeek123", appContext.packageName)
     }
 }
